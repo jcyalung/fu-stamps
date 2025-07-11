99a3824ca1b83c53a00f33189adaa00d1e8abc90
Harrison Tran <harrit4@uci.edu>
2025-07-10 22:54:09 -0700

    added try catch block to wrap api call and edited regex

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,51 +5,57 @@ import { sign } from "jsonwebtoken";
 const MAX_AGE = 60*60*24*7; // max age set for 7 days
 
 export async function POST(request: Request) {
-    // getting the request body and storing the values in variables
-    const { email, password, verification, user_id } = await request.json();
+    try {
+        // getting the request body and storing the values in variables
+        const { email, password, verification, user_id } = await request.json();
 
-    //regex for email validation
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        //regex for email validation
+        const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.(com|edu|gov)$/;
 
-    if (!emailRegex.test(email)) {
-        return NextResponse.json({ message: "Invalid email" }, { status: 500 });
+        if (!emailRegex.test(email)) {
+            return NextResponse.json({ message: "Invalid email" }, { status: 500 });
+        }
+
+        // sample data login
+        if ( email !== "admin@gmail.com" ) {
+            return NextResponse.json({ message: "Invalid email" }, { status: 404 });
+        }
+        else if ( password !== "password" ) {
+            return NextResponse.json({ message: "Invalid password" }, { status: 401 });
+        }
+        else if ( verification !== 2 ) {
+            return NextResponse.json({ message: "User is not verified" }, { status: 500 });
+        }
+        
+        const secret = process.env.JWT_SECRET || "";
+
+        // generate json web token signed by secret
+        const token = sign(
+            { email, user_id },
+            secret,
+            { expiresIn: MAX_AGE }
+        );
+
+        const serialized = serialize("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: MAX_AGE,
+            path: "/",
+        });
+
+        const response = {
+            message: "Login successful",
+        }
+
+        return new Response(JSON.stringify(response), {
+            status: 200,
+            headers: {
+                "Set-Cookie": serialized,
+            },
+        });
     }
-
-    // sample data login
-    if ( email !== "admin@gmail.com" ) {
-        return NextResponse.json({ message: "Invalid email" }, { status: 404 });
+    catch (error: unknown) {
+        return NextResponse.json({message: 'a'}, {status : 500})
     }
-    else if ( password !== "password" ) {
-        return NextResponse.json({ message: "Invalid password" }, { status: 401 });
-    }
-    else if ( verification !== 2 ) {
-        return NextResponse.json({ message: "User is not verified" }, { status: 500 });
-    }
-    
-    const secret = process.env.JWT_SECRET || "";
-
-    const token = sign(
-        { email, user_id },
-        secret,
-        { expiresIn: MAX_AGE }
-    );
-
-    const serialized = serialize("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: MAX_AGE,
-        path: "/",
-    });
-
-    const response = {
-        message: "Login successful",
-    }
-
-    return new Response(JSON.stringify(response), {
-        status: 200,
-        headers: {
-            "Set-Cookie": serialized,
-        },
-    });
 }
