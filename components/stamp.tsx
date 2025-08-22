f9fb0b2377c06540dc55dbd3d805bceb434d9bda
Harrison Tran <harrit4@uci.edu>
2025-08-21 18:42:51 -0700

    word will be based on input

--- components/stamp.tsx
+++ components/stamp.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { Montserrat } from "next/font/google";
+import { useState } from "react";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -8,7 +11,24 @@ const montserrat = Montserrat({
 });
 
 export default function Stamp(){
-    const word = "SNOW"; // example word
+    // fade in and out the status message
+    const [ status, setStatus ] = useState("hidden");
+    const [ fade, setFade ] = useState(false);   
+
+    const [word, setWord] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSetWord = () => {
+        setStatus("flex");
+        setFade(true);
+
+        setMessage(`THE WORD OF THE DAY "${word}" HAS BEEN SET`);
+
+        setTimeout(() => {
+            setFade(false);
+        }, 3000);
+    }
+
     return (
         <div className="flex flex-col items-center bg-white text-black w-[542px] h-[210px] border-1 border-b-6 border-r-4 p-[32px]">
             <div className="flex flex-col gap-y-[8px]">
@@ -16,16 +36,18 @@ export default function Stamp(){
                     ENTER THE "WORD OF THE DAY"
                 </div>
                 <div className="flex w-[478px] h-[36px] border-1 rounded-4xl bg-lightyellow items-center">
-                    <input type="text" className={`ml-[4px] h-[20px] input rounded-full bg-lightyellow focus:outline-none ${montserrat.className} font-light`} />
+                    <input onChange={(e) => setWord(e.target.value)} type="text" className={`ml-[4px] h-[20px] input rounded-full bg-lightyellow focus:outline-none ${montserrat.className} font-light`} />
                 </div>
             </div>
             <div>
-                <button className={`btn h-[56px] w-[161px] mt-[20px] border-b-4 border-r-2 rounded-none bg-amber-400 px-[24px] py-[16px] text-lg text-black ${montserrat.className}`}>
+                <button onClick={handleSetWord} className={`btn h-[56px] w-[161px] mt-[20px] border-b-4 border-r-2 rounded-none bg-amber-400 px-[24px] py-[16px] text-lg text-black ${montserrat.className}`}>
                     SET WORD
                 </button>
             </div>
-            <div className={`flex ${montserrat.className} italic bg-amber-400 w-[685px] h-[87px] mt-[90px] p-[24px] justify-center border-1 border-b-6 border-r-4 text-[26px]`}>
-                THE WORD OF THE DAY "{word}" HAS BEEN SET
+            {/* in the future this will be conditionally rendered based on the token */}
+            <div className={status + ` ${montserrat.className} italic bg-amber-400 w-[685px] mt-[90px] p-[24px] justify-center border-1 border-b-6 border-r-4 text-[26px]
+                            transition-all ease-in-out duration-500 ${fade ? "opacity-100" : "opacity-0"}`}>
+                {message}
             </div>
             <div className={`hidden ${montserrat.className} italic bg-amber-400 w-[740px] h-[87px] mt-[90px] p-[24px] justify-center border-1 border-b-6 border-r-4 text-[26px]`}>
                 THE WORD OF THE DAY "{word}" FAILED TO BE SET
