9d68431ef9ab3d602f7e9368873ed35a197d9783
Kailee Kaocharoen <kaileeteakao@gmail.com>
2025-09-15 13:29:30 -0700

    styling fixed for textbox

--- app/components/TitleTextbox/TitleTextbox.tsx
+++ app/components/TitleTextbox/TitleTextbox.tsx
@@ -7,8 +7,18 @@ interface TitleTextboxProps {
 
 const TitleTextbox: React.FC<TitleTextboxProps> = ({ children, className = "" }) => {
     return (
-    <div
-      className={`px-20 py-1 bg-amber-400 shadow-[4px_6px_0px_0px_rgba(0,0,0,1)] outline outline-1 outline-offset-[-1px] outline-black inline-flex justify-end items-center overflow-hidden ${className}`}
+    <div 
+    style={{
+      display: 'inline-flex',
+      padding: '4px 80px',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      margin: '60px', // temporary while other items aren't on page
+      
+      border: '1px solid #000000',
+      backgroundColor: '#FBCA29',
+      boxShadow: '4px 6px 0 0 #000000'
+      }}
     >
       <div className="text-center text-white text-6xl font-bold font-['Jost'] tracking-[3.20px]">
         {children}
