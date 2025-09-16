b61316c31e266b0cdaf3ab1f9af64139420d1a94
jcyalung <jyalung1@uci.edu>
2025-09-16 12:20:52 -0700

    fix createStampCard

--- app/api/createStampCard/route.ts
+++ app/api/createStampCard/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from 'next/server';
 
 const supabase = createClient(
   process.env.SUPABASE_URL || "",
-  process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+  process.env.SUPABASE_ANON_KEY || ""
 );
 
 export async function GET(request: Request) {
