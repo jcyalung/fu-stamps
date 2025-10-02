62b379770e8596fd02c56535f01a16dd2a15f4cb
jcyalung <jyalung1@uci.edu>
2025-10-02 11:40:22 -0700

    montserrat changes

--- components/root/Section2.tsx
+++ components/root/Section2.tsx
@@ -1,10 +1,9 @@
-import { montserrat_global as montserrat } from "@/constants";
 export function StampProto(){
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { e.preventDefault(); };
     return (
         <form onSubmit={handleSubmit} className="flex flex-col items-center bg-white text-black w-[542px] h-[210px] border-1 border-b-6 border-r-4 p-[32px]">
             <div className="flex flex-col gap-y-[8px]">
-                <label htmlFor="stamp" className={`text-xs ${montserrat.className} font-light `}>
+                <label htmlFor="stamp" className={`text-xs font-light `}>
                     ENTER THE "WORD OF THE DAY"
                 </label>
                 <div className="flex w-[478px] h-[36px] border-1 rounded-4xl bg-lightyellow items-center">
@@ -12,12 +11,12 @@ export function StampProto(){
                     type="text"
                     id="stamp"
                     name="stamp"
-                    className={`ml-[4px] h-[20px] input rounded-full bg-lightyellow focus:outline-none ${montserrat.className} font-light`}
+                    className={`ml-[4px] h-[20px] input rounded-full bg-lightyellow focus:outline-none font-light`}
                     />
                 </div>
             </div>
             <div>
-                <button type='submit' className={`btn h-[56px] w-[161px] mt-[20px] border-b-4 border-r-2 rounded-none bg-amber-400 px-[24px] py-[16px] text-lg text-black ${montserrat.className}`}>
+                <button type='submit' className={`btn h-[56px] w-[161px] mt-[20px] border-b-4 border-r-2 rounded-none bg-amber-400 px-[24px] py-[16px] text-lg text-black`}>
                     SET WORD
                 </button>
             </div>
@@ -30,16 +29,16 @@ export function StampCardProto() {
 
   return (
     <div className="bg-yellow-400 p-6 rounded-2xl shadow-lg w-[300px]">
-        <p className={`${montserrat.className}`}>this is the stampcard proto</p>
+        <p>this is the stampcard proto</p>
     </div>
   );
 }
 
 export function HowItWorks() {
   return (
-    <div className={`${montserrat.className} flex flex-col items-center bg-white text-black w-[960px] h-[580px] border-1 border-b-6 border-r-4 p-[32px]`}>
-      <h2 className={`${montserrat.className} text-3xl font-bold drop-shadow-md`}>HOW IT WORKS</h2>
-      <ol className={`${montserrat.className} space-y-4 text-black mt-10`}>
+    <div className={`flex flex-col items-center bg-white text-black w-[960px] h-[580px] border-1 border-b-6 border-r-4 p-[32px]`}>
+      <h2 className={`text-3xl font-bold drop-shadow-md`}>HOW IT WORKS</h2>
+      <ol className={`space-y-4 text-black mt-10`}>
         <li className='italic'><span className="font-semibold">Step 1:</span> Go to study hours online or in-person</li>
         <li className='italic'><span className="font-semibold">Step 2:</span> Log into your FU-Stamps account</li>
         <li className='italic'><span className="font-semibold">Step 3:</span> Enter the “Word of the Day” or check in with a director</li>
