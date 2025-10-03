af3dbc880839e5ca03e54111c084229f64cf0742
Harrison Tran <harrit4@uci.edu>
2025-10-03 00:02:51 -0700

    styling and arrangement of components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image";
 import page_image from '@/components/images/landing_page_section.png';
-import TitleTextbox from "@/components/TitleTextbox";
+import logo from '@/components/root/fusion_logo.png';
 import ModalSelector from "@/components/root/ModalSelector";
 import LogoBackground from "@/components/root/LogoBackground";
 import RewardBox from '@/components/root/reward_box.png';
@@ -61,28 +61,34 @@ export default function Home() {
 
       
       <div className="relative w-full min-h-screen bg-lightyellow">
-        
-        <LogoBackground />
-
-        {/* ts don't work so we'll get back to it later
-        <div className="relative flex flex-col md:flex-row items-start md:items-center w-full h-full px-8 md:px-16">
-          <div className="flex flex-col gap-16 md:w-1/2 mt-20">
-            <StampProto />
-            <HowItWorks />
-          </div>
-
-          <div className="flex flex-col items-center gap-16 md:w-1/2 mt-20">
-            <Image
-              src={RewardBox}
-              alt="hot_dog_reward"
-              className="p-6 rounded-2xl shadow-lg"
-            />
-            <div className="mt-auto">
+            <div className="absolute left-[33vw] bottom-[16px] z-10">
+              <StampProto />
+            </div>
+            <div className="absolute left-[36vw] top-[17vh] z-10">
+              <HowItWorks />
+            </div>
+            <div className="absolute bottom-0 left-[79vw] z-10">
+              <Image
+                src={RewardBox}
+                alt="hot_dog_reward"
+                className="p-6 rounded-2xl"
+              />
+            </div>
+            <div className="absolute left-[13vw] top-[11vh] z-3">
               <StampCardProto />
             </div>
-
-          </div> 
-        </div> */}
+            <div className="absolute left-[3vw] top-[28vh] z-1">
+              <StampCardProto
+                color="white"
+                claim={false}/>
+            </div>
+            <div className="absolute left-[13vw] top-0 w-[56vw] h-[56vw]">
+              <Image
+                src={logo}
+                alt="logo"
+                fill
+              />
+            </div>
       </div>
       
 
--- components/root/Section2.tsx
+++ components/root/Section2.tsx
@@ -1,8 +1,11 @@
+import HOWITWORKS from "@/components/root/HOW IT WORKS.png"; 
+import Image from "next/image";
 export function StampProto(){
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { e.preventDefault(); };
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col items-center bg-white text-black w-[542px] h-[210px] border-1 border-b-6 border-r-4 p-[32px]">
-            <div className="flex flex-col gap-y-[8px]">
+        <form onSubmit={handleSubmit} className="relative flex flex-col items-center bg-white text-black w-[542px] h-[210px] border-1 border-b-6 border-r-4 p-[32px]">
+            <h2 className="absolute right-[15px] top-[15px] text-black">&#10006;</h2>
+            <div className="flex flex-col gap-y-[8px]">                
                 <label htmlFor="stamp" className={`text-xs font-light `}>
                     ENTER THE "WORD OF THE DAY"
                 </label>
@@ -11,38 +14,57 @@ export function StampProto(){
                     type="text"
                     id="stamp"
                     name="stamp"
-                    className={`ml-[4px] h-[20px] input rounded-full bg-lightyellow focus:outline-none font-light`}
+                    placeholder="MAGIC WORD"
+                    className={`ml-[4px] h-[20px] input rounded-full bg-lightyellow focus:outline-none font-light placeholder-black`}
                     />
                 </div>
             </div>
             <div>
-                <button type='submit' className={`btn h-[56px] w-[161px] mt-[20px] border-b-4 border-r-2 rounded-none bg-amber-400 px-[24px] py-[16px] text-lg text-black`}>
-                    SET WORD
+                <button type='submit' className={`btn h-[56px] w-[161px] mt-[20px] border-b-4 border-r-2 rounded-none bg-amber-400 py-[16px] text-lg text-black`}>
+                    ADD STAMP
                 </button>
             </div>
         </form>
     )
 }
 
-export function StampCardProto() {
-  const stamps = Array(10).fill("This Long Word");
+export function StampCardProto({color="yellow-400", claim=true}) {
+  const stamps = Array(10).fill(
+    <div className="w-[257px] h-[22px] flex justify-center">
+      <div className="flex flex-row gap-[32px]">
+        <div className="flex items-center justify-center rounded-4xl border-1 bg-lightyellow font-montserrat text-[10px] text-black px-[10px]">THIS LONG WORD</div>
+        <div className="flex items-center justify-center rounded-4xl border-1 bg-lightyellow font-montserrat w-[98px] text-xs text-black">2025-07-12</div>
+      </div>
+    </div>
+  );
 
   return (
-    <div className="bg-yellow-400 p-6 rounded-2xl shadow-lg w-[300px]">
-        <p>this is the stampcard proto</p>
+    <div className={`relative flex items-center bg-${color} shadow-lg w-[344px] h-[580px] border-1 border-b-6 border-r-4 border-black flex flex-col items-center`}>
+        <h2 className="absolute right-[15px] top-[15px] text-black">&#10006;</h2>
+        <p className="flex flex-col gap-[25px] mt-[46px]">{stamps}</p>
+        { claim ? 
+        <div className="bg-lightyellow w-[166px] h-[37px] flex justify-center items-center text-black font-montserrat text-lg font-semibold border-1 border-b-4 border-r-2 mt-[25px]">
+          CLAIM PRIZE
+        </div>
+        :
+        <div className="bg-[#FBCA29] w-[166px] h-[37px] flex justify-center items-center text-black font-montserrat text-lg font-semibold border-1 border-b-4 border-r-2 mt-[25px]">
+          ADD STAMP
+        </div>
+        }
+
     </div>
   );
 }
 
 export function HowItWorks() {
   return (
-    <div className={`flex flex-col items-center bg-white text-black w-[960px] h-[580px] border-1 border-b-6 border-r-4 p-[32px]`}>
-      <h2 className={`text-3xl font-bold drop-shadow-md`}>HOW IT WORKS</h2>
-      <ol className={`space-y-4 text-black mt-10`}>
-        <li className='italic'><span className="font-semibold">Step 1:</span> Go to study hours online or in-person</li>
-        <li className='italic'><span className="font-semibold">Step 2:</span> Log into your FU-Stamps account</li>
-        <li className='italic'><span className="font-semibold">Step 3:</span> Enter the “Word of the Day” or check in with a director</li>
-        <li className='italic'><span className="font-semibold">Step 4:</span> Collect 10 stamps → claim your prize (QR code coupon)</li>
+    <div className={`flex flex-col items-center bg-white text-black w-[960px] h-[580px] border-1 border-b-6 border-r-4 p-[32px] opacity-90`}>
+      <Image src={HOWITWORKS} alt="How It Works" className="w-[546px] h-auto" />
+      <ol className={`space-y-10 text-black mt-10 text-3xl uppercase font-extralight`}>
+        <li className='italic'><span>Step 1:</span> Go to study hours online or in-person</li>
+        <li className='italic'><span>Step 2:</span> Log into your FU-Stamps account</li>
+        <li className='italic'><span>Step 3:</span> Enter the “Word of the Day” or check in with a director</li>
+        <li className='italic'><span>Step 4:</span> Collect 10 stamps → claim your prize (QR code coupon)</li>
       </ol>
     </div>
   );
