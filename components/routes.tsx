f0712fb36e09dfa4d6eb0c3dfe0fa7dbaa9d124c
jcyalung <jyalung1@uci.edu>
2025-10-06 16:34:08 -0700

    route change

--- components/routes.tsx
+++ components/routes.tsx
@@ -25,11 +25,11 @@ export const ROUTE_ACCESS: { pattern: RegExp; minLevel: number}[] = [
     { pattern: /^\/profile/, minLevel: 1},
     { pattern: /^\/stampcard/, minLevel: 1},
     { pattern: /^\/api\/addStamp$/,  minLevel: 1},
-    { pattern: /^\/api\/deleteStamp$/,  minLevel: 1},
     { pattern: /^\/api\/createStampCard$/,  minLevel: 1},
-
+    
     // academics director routes
     { pattern: /^\/stamp/, minLevel: 2},
     { pattern: /^\/attendance/, minLevel: 2},
     { pattern: /^\/api\/setStamp$/,  minLevel: 2},
+    { pattern: /^\/api\/deleteStamp$/,  minLevel: 2},
 ]
