0ece072776c686b070bf2cded4d17d4df7afc0e3
Harrison Tran <harrit4@uci.edu>
2025-10-07 00:58:31 -0700

    top row align

--- components/Stampcard.tsx
+++ components/Stampcard.tsx
@@ -16,18 +16,30 @@ export type StampProps = {
 */
 export default function Stampcard({stamps} : StampProps) {
     return( 
-        <div className="relative w-[400px] h-[250px]">
+        <div className="relative w-[28vw] h-auto">
             <Image
               src={design}
               alt="Photo"
               className="object-cover rounded-lg"
               />
-            <div className="absolute inset-0 flex items-center justify-center">
+            <div className="absolute left-[2vw] w-[24vw] bottom-[12vw] flex flex-row justify-between">
               {/* please tinker with the width and height. 
                   please also make 10 copies for each stamp date, and comment the offsets for each date in the comments of your github request.
               
               */}
-              <p className="text-black text-xl font-bold px-4 py-2 rounded">
+              <p className="text-black text-[.6vw] w-[3.8vw] text-center font-bold px-4 rounded border-1 h-[2vw]">
+                mm-dd
+              </p>
+              <p className="text-black text-[.6vw] w-[3.8vw] text-center font-bold px-4 rounded border-1 h-[2vw]">
+                mm-dd
+              </p>
+              <p className="text-black text-[.6vw] w-[3.8vw] text-center font-bold px-4 rounded border-1 h-[2vw]">
+                mm-dd
+              </p>
+              <p className="text-black text-[.6vw] w-[3.8vw] text-center font-bold px-4 rounded border-1 h-[2vw]">
+                mm-dd
+              </p>
+              <p className="text-black text-[.6vw] w-[3.8vw] text-center font-bold px-4 rounded border-1 h-[2vw]">
                 mm-dd
               </p>
             </div>
